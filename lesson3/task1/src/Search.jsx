4bcaaf88fa3e0faff43bbacc2e749e38f1b236ec
annaKulyk <ankakulik1998@gmail.com>
2021-10-07 15:26:16 +0300

    functional component

---
+++ lesson3/task1/src/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './search.scss';
+
+export const Search = ({ name }) => (
+  <div className="search">
+    <h1 className="search__title">Hello, {name}. What to search for you?</h1>
+    <div className="search__field">
+      <input type="text" className="search__input" />
+      <button className="search__button">Search</button>
+    </div>
+  </div>
+);
