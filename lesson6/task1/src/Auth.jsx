3cc3d7f140ea4fee5aa59fa8c7153bf5c633ad21
annaKulyk <ankakulik1998@gmail.com>
2021-10-12 20:06:55 +0300

    Conditional rendering

---
+++ lesson6/task1/src/Auth.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Logout from './Logout';
+import Login from './Login';
+import Greeting from './Greeting';
+
+class Auth extends React.Component {
+  state = {
+    isLoggedIn: false,
+  };
+
+  handleLogin = () =>
+    this.setState({
+      isLoggedIn: true,
+    });
+
+  handleLogout = () =>
+    this.setState({
+      isLoggedIn: false,
+    });
+
+  render() {
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+        {this.state.isLoggedIn ? (
+          <Logout onLogout={this.handleLogout} />
+        ) : (
+          <Login onLogin={this.handleLogin} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ lesson6/task1/src/Greeting.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import UserGreeting from './UserGreeting';
+import GuestGreeting from './GuestGreeting';
+
+const Greeting = ({ isLoggedIn }) => (isLoggedIn ? <UserGreeting /> : <GuestGreeting />);
+
+export default Greeting;
