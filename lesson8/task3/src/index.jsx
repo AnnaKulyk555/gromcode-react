66d2702ba9c88e2cb7d9003e4eebbb3101936434
annaKulyk <ankakulik1998@gmail.com>
2021-10-17 14:42:09 +0300

    reneme Dimensions.jsx

--- lesson8/task3/src/index.jsx
+++ lesson8/task3/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Dimensions from './Demensions';
+import Dimensions from './Dimensions';
 
 const rootElement = document.querySelector('#root');
 
