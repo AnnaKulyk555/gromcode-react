40ee97574d9cfe60b03f864f0b5f169e037932d3
annaKulyk <ankakulik1998@gmail.com>
2021-10-20 18:32:09 +0300

    lifting state up

---
+++ lesson10/task1/src/UserProfile.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const UserProfile = ({ userData }) => {
+  const { name, location, avatar_url } = userData;
+
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
