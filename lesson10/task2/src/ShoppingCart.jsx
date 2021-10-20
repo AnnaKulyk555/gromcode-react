f32c904abfb7bb872a70e0a457f88750155243a0
annaKulyk <ankakulik1998@gmail.com>
2021-10-20 21:42:23 +0300

    products list

---
+++ lesson10/task2/src/ShoppingCart.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import CartTitle from './CartTitle';
+import ProductsList from './ProductsList';
+
+class ShoppingCart extends React.Component {
+  state = {
+    cartItems: [
+      { id: '1', name: 'iPad', price: 799 },
+      { id: '2', name: 'iPad Pro', price: 1099 },
+      { id: '3', name: 'iPad Pro Pro', price: 3000 },
+    ],
+  };
+
+  render() {
+    const count = this.state.cartItems.length;
+
+    return (
+      <div className="column">
+        <CartTitle userName={this.props.userData.firstName} count={count} />
+        <ProductsList cartItems={this.state.cartItems} />
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
