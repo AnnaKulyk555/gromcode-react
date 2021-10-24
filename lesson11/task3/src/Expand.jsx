e91e226e673743bbf488d279faf17082ae0f4642
annaKulyk <ankakulik1998@gmail.com>
2021-10-24 16:32:02 +0300

    wrote logic in Expand component

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,34 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Expand = ({ children, title, isOpen, onOpen, onClose }) => (
-  <div className="expand border">
-    <div className="expand__header">
-      <span className="expand__title">{title}</span>
-      {isOpen ? (
-        <button className="expand__toggle-btn" onClick={onClose}>
-          <i className="fas fa-chevron-up"></i>
-        </button>
-      ) : (
-        <button className="expand__toggle-btn" onClick={onOpen}>
-          <i className="fas fa-chevron-down"></i>
-        </button>
-      )}
-    </div>
-    {isOpen ? <div className="expand__content">{children}</div> : null}
-  </div>
-);
+class Expand extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  hideContent = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  showContent = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    const { title, children } = this.props;
+
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          {isOpen ? (
+            <button className="expand__toggle-btn" onClick={this.hideContent}>
+              <i className="fas fa-chevron-up"></i>
+            </button>
+          ) : (
+            <button className="expand__toggle-btn" onClick={this.showContent}>
+              <i className="fas fa-chevron-down"></i>
+            </button>
+          )}
+        </div>
+        {isOpen ? <div className="expand__content">{children}</div> : null}
+      </div>
+    );
+  }
+}
 
 Expand.propTypes = {
-  isOpen: PropTypes.bool,
   children: PropTypes.element.isRequired,
   title: PropTypes.string,
-  onClose: PropTypes.func.isRequired,
-  onOpen: PropTypes.func.isRequired,
 };
 
 Expand.defaultProps = {
-  isOpen: false,
   title: '',
 };
 
